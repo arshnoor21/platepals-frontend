a2d5c40408184cbe8a3aed2071ec8460038a0d37
arshnoor <arshnoor@tamu.edu>
2024-04-06 21:16:46 -0500

    Init commit

---
+++ src/components/CreateRoom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CreateRoom() {
+    return (
+        <div>
+            <h1>Create Room</h1>
+            {/* Your form for creating a room */}
+        </div>
+    );
+}
+
+export default CreateRoom;
---
+++ src/components/JoinRoom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function JoinRoom() {
+    return (
+        <div>
+            <h1>Join Room</h1>
+            {/* Your form for joining a room */}
+        </div>
+    );
+}
+
+export default JoinRoom;
